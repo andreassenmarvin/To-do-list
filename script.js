52ba8d7323a0a6f3a8f0bd2f6ad2ce95822a5e77
Marvin Maina <machariamarvin625@gmail.com>
2021-07-29 18:48:32 +0300

    Add sticky navigation bar

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 //Navbar that displays on scroll
+var nav = document.getElementById("navbar")
 
-$(function(){
-    $("#navbar")
+var scrollWindow = $(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300){
+            $(nav).css("top", 0).css("background-color", "white");
+            $(nav).addClass("nav-animation");
+        }
+        else if ($(window).scrollTop() < 300){
+            $(nav).css("top", 0).css("background-color", "transparent");
+            $(nav).removeClass("nav-animation");
+            $(nav).addClass("animation-nav");
+        }
+        else {
+            
+        }
+    })
 })
+
+scrollWindow()
